662dd7e47e416f9ec52f641dd087eb25d8ce7ef2
nouarmohamed <mohamed.mohnouar@gmail.com>
2025-07-16 00:09:20 +0100

    fix typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const IBMPlexSans = localFont({
   src: [
     {path: "./fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal",},
     {path: "./fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal",},
-    {path: "./fonts/IBMPlexSans-Semibold.ttf", weight: "600", style: "normal",},
+    {path: "./fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal",},
     {path: "./fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal",},
   ],
 });
